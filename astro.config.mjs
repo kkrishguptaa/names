9c34a51ff9cb03028d7b698907d06f2666948ce0
Krish Gupta <m.krishggupta@icloud.com>
2025-06-21 11:22:43 +0530

    chore: setup astro react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
+import partytown from '@astrojs/partytown';
+import sentry from '@sentry/astro';
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), partytown(), sentry(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
